79a9967883776a5a6004d681c4786116703dcacc
ranaahmed <rnaahmd240@gmail.com>
2025-10-29 20:52:41 +0300

    FULL CRUD FOR LIST INGREDIENTS

--- src/components/Ingredients/IngredientsList.jsx
+++ src/components/Ingredients/IngredientsList.jsx
@@ -24,13 +24,47 @@ export default function () {
         loadIngredaients()
         [refresh]
     })
-    const handleDelete = async(id)=>{
+    const handleDelete = async (id) =>{
+        try{
+            await axios.delete(`${API_URL}${id}/`)
+            setRefresh(!refresh)
+        }catch{
+            setError("can't delete ingredients")
+        }
 
     }
-
-
+    const handleEdit =(item) =>{
+        setEditItem(item)
+    }
+    const handleUpdate = async(updateData)=>{
+        try{
+            await axios.put(`${API_URL}${editItem.id}/`,updateData)
+            setEditItem(null)
+            setRefresh(!refresh)
+        }catch{
+            setError("update filed")
+        }
+    }
   return (
-    <div>
+    <div className="ingredients-container">
+        <h2>Ingredients</h2>
+        <IngredientsForm
+        onSuccess={()=>
+            setRefresh(!refresh)}
+            editItem={editItem}
+            onUpdate={handleUpdate}
+        />
+        <ul>
+            {ingredients.map((i)=>(
+                <li key ={i.id}>
+                    <strong>{i.name}</strong> {i.cost_per_unit} per {i.unit}
+                    <button onClick={() => handleEdit(i)}> Edit</button>
+                    <button onClick={() => handleDelete(i.id)}>Delete</button>
+                </li>
+            )
+            )}
+        </ul>
+
     
     </div>
   )
