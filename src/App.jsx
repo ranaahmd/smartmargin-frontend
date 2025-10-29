17f3ef80b1259054e947f77d455227c57e7fa868
ranaahmed <rnaahmd240@gmail.com>
2025-10-29 14:37:32 +0300

    update login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import SignUp from "./components/Auth/Signup";
 import Login from "./components/Auth/Login";
-
+//  i need to protcet router
 
 export default function App() {
   return (
@@ -10,7 +10,7 @@ export default function App() {
        <Route path="/" element={<div>Home Page - <a href="/signup">Sign Up</a> | <a href="/login">Login</a></div>} />
       <Route path ="/signup" element={<SignUp/>}/>
       <Route path ="/login" element={<Login/>}/>
-      <Route path ="/dashboard" element={<Dashboard/>}/>
+      {/* <Route path ="/dashboard" element={<Dashboard/>}/> */}
     </Routes>
    </Router>
   )
